b1a13045dfe6f2e3cb7f10477eb45f5ba7566c67
Gumelevskii <y.gume1ev@gmail.com>
2022-06-24 00:30:53 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
 }
