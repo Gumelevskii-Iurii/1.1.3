9c64ac5d26aeac33dda82f8330836613420bd95a
Gumelevskii <y.gume1ev@gmail.com>
2022-06-26 18:34:06 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -19,33 +19,30 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
 
-       try (Statement stmt = Util.getConnection().createStatement()){
-           stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(64), lastname VARCHAR(64), age INT)");
-
-       }  catch (SQLException e) {
-           e.printStackTrace();
-       }
+        try (Statement stmt = Util.getConnection().createStatement()) {
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(64), lastname VARCHAR(64), age INT)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
         try (Statement stmt = Util.getConnection().createStatement()) {
             stmt.executeUpdate("DROP TABLE IF EXISTS users ");
-        }  catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
-        try (PreparedStatement pstmt = Util.getConnection().prepareStatement("INSERT INTO users (name, lastname, age) VALUES (?, ?, ?)")){
+        try (PreparedStatement pstmt = Util.getConnection().prepareStatement("INSERT INTO users (name, lastname, age) VALUES (?, ?, ?)")) {
             pstmt.setString(1, name);
             pstmt.setString(2, lastName);
             pstmt.setInt(3, age);
-
             pstmt.executeUpdate();
-
             System.out.println("User with name – " + name + " добавлен в базу данных");
-        }  catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
@@ -53,11 +50,11 @@ public class UserDaoJDBCImpl implements UserDao {
     public void removeUserById(long id) {
         String string = "DELETE FROM users WHERE ID = ?";
 
-        try (PreparedStatement pstmt = Util.getConnection().prepareStatement(string)){
+        try (PreparedStatement pstmt = Util.getConnection().prepareStatement(string)) {
             pstmt.setLong(1, id);
             pstmt.executeUpdate();
 
-        }  catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
@@ -65,7 +62,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
         String string2 = "SELECT * FROM users";
-        try (PreparedStatement pstmt = Util.getConnection().prepareStatement(string2)){
+        try (PreparedStatement pstmt = Util.getConnection().prepareStatement(string2)) {
             ResultSet resultSet = pstmt.executeQuery(string2);
             while (resultSet.next()) {
                 User user = new User();
@@ -73,12 +70,10 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setName(resultSet.getString("name"));
                 user.setLastName(resultSet.getString("lastname"));
                 user.setAge(resultSet.getByte("age"));
-
                 users.add(user);
-
             }
             pstmt.executeQuery();
-        }  catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
@@ -88,7 +83,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (Statement stmt = Util.getConnection().createStatement()) {
             stmt.execute("TRUNCATE TABLE users");
-        }  catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
