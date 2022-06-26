b4e0260df50d084a9b8a3060d7fd327708c8cdd0
Gumelevskii <y.gume1ev@gmail.com>
2022-06-26 20:25:25 +0300

    Commit

--- src/test/java/MyclassTest.java
+++ src/test/java/MyclassTest.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class MyclassTest {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Wow");
     }
 }
