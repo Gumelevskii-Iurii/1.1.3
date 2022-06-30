f0e68b7642ca0077d03a8f239231ca482b7fd07c
Gumelevskii <y.gume1ev@gmail.com>
2022-06-30 17:05:21 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,15 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
 
         // реализуйте алгоритм здесь
-        Util.getSessionFactory();
 
         UserServiceImpl userService = new UserServiceImpl();
 
         userService.createUsersTable();
-        System.out.println("OK");
 
         userService.saveUser("Name1", "LastName1", (byte) 20);
         userService.saveUser("Name2", "LastName2", (byte) 25);
